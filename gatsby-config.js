f62b5a43787a9cbf2d7fadeaa39ddd4112b6b720
Patrick Levy <patricklevy@exosite.com>
2020-04-06 08:28:13 -0500

    Adjust color

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#5a6aa0d4`,
+        theme_color: `#5a6aa0d4`,
         display: `minimal-ui`,
         icon: `src/images/erlenmeyer-flask.png`, // This path is relative to the root of the site.
       },
